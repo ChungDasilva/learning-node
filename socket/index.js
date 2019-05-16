299f6043b0f360c425c70402ded354284c57783b
Nguyen Ha Chung <nguyen.ha.chung@captcha.com>
2019-05-16 12:40:32 +0900

    add

--- socket/index.js
+++ socket/index.js
@@ -10,11 +10,38 @@ var io = require("socket.io")(server);
 server.listen(3000);
 
 io.on("connection", function(socket) {
+	// show danh sách room đang có
+	console.log(socket.adapter.rooms);
    console.log("co nguoi ket noi" + socket.id);
 
    socket.on("disconnect", function() {
    		console.log(socket.id + " ngat ket noi");
    });
+
+   socket.on("client-send-data", function (data) {
+   		console.log(socket.id + " vua gui:" +data);
+   		// -> send về cho tất cả client
+   		//io.sockets.emit("server-send-data", "server da nhan dc");
+   		// -> send về cho 1 client mà gửi đến
+   		//socket.emit("server-send-data", "server da nhan dc");
+   		// -> send về cho các client khác mà không phải client gửi đến server
+   		socket.broadcast.emit("server-send-data", "server da nhan dc");
+   		// -> send đến client chỉ định
+   		//io.to("socketid").emit();
+   })
+
+   socket.on("create-room", function (room) {
+   	socket.join(room);
+   	socket.phong = room;
+   	var mang = [];
+   	for (r in socket.adapter.rooms) {
+   		mang.push(r);
+   	}
+   	io.sockets.emit("server-room", mang);
+   	//send đến client trong room
+   	io.sockets.in(socket.phong).emit("chat-room", room)
+   	console.log(socket.phong)
+   })
 });
 
 app.get("/", function(req, res) {
